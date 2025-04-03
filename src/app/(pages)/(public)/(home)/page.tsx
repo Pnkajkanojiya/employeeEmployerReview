faee0f044c6f4397631b77b7157fa8910cdbd913
Sibi Abraham <sibi@geeconsystems.com>
2025-04-03 19:16:56 +0530

    fix: removed npm setup

--- src/app/(pages)/(public)/(home)/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import FigmaEmbed from "../../(auth)/componets/FigmaEmbed";
-import Login from "../../(auth)/componets/FigmaEmbed";
-
-export default function Home() {
-  return (
-    <Login/>
-  );
-}
