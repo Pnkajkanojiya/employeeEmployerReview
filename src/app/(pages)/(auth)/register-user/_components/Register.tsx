7c15422c3ca1cc2a7d581390a48e2dc0302d5bfa
nandu-2023 <nandusankhe@gmail.com>
2025-04-07 11:00:20 +0530

    feat: add layout wrapper component

---
+++ src/app/(pages)/(auth)/register-user/_components/Register.tsx
@@ -0,0 +1,185 @@
+'use client';
+import AuthPageLayout from '@/common/components/layouts/AuthPageLayout';
+import Link from 'next/link';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+
+const Register: React.FC = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>('');
+  const [fullName, setFullName] = useState<string>('');
+  const [mobileNumber, setMobileNumber] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [rememberMe, setRememberMe] = useState<boolean>(false);
+  console.log(setShowPassword);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    // Validate if passwords match
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+    console.log({ email, fullName, mobileNumber, password, rememberMe });
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setEmail(e.target.value);
+  };
+
+  const handleFullNameChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setFullName(e.target.value);
+  };
+
+  const handleMobileNumberChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setMobileNumber(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleRememberMeChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setRememberMe(e.target.checked);
+  };
+
+  return (
+    <AuthPageLayout>
+      <div className="flex flex-col md:flex-row">
+        <div className="w-[90%] md:w-1/2">
+          <div className="h-full flex flex-col justify-center max-w-md mx-auto mt-[10px]">
+            <h2 className="text-2xl font-bold text-black mb-[5px]">Register</h2>
+            <p className="text-gray-900 text-sm mb-[10px] whitespace-nowrap">
+              Enter your details to create an account
+            </p>
+
+            <form onSubmit={handleSubmit} className="space-y-2">
+              <div className="mb-2">
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  placeholder="Enter your email"
+                  className="px-3 py-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                  required
+                />
+              </div>
+
+              <div className="mb-2">
+                <label htmlFor="fullName" className="block text-sm font-medium text-gray-700 mb-1">
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  id="fullName"
+                  value={fullName}
+                  onChange={handleFullNameChange}
+                  placeholder="Enter your full name"
+                  className="px-3 py-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                  required
+                />
+              </div>
+
+              <div className="mb-2">
+                <label
+                  htmlFor="mobileNumber"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Mobile Number
+                </label>
+                <input
+                  type="text"
+                  id="mobileNumber"
+                  value={mobileNumber}
+                  onChange={handleMobileNumberChange}
+                  placeholder="Enter your mobile number"
+                  className="px-3 py-2 border border-gray-300 rounded-md w-full focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                  required
+                />
+              </div>
+
+              <div className="mb-2">
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
+                  Password
+                </label>
+                <div className="relative">
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    id="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    placeholder="Enter your password"
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="mb-2">
+                <label
+                  htmlFor="confirmPassword"
+                  className="block text-sm font-medium text-gray-700 mb-1"
+                >
+                  Confirm Password
+                </label>
+                <div className="relative">
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    value={confirmPassword}
+                    onChange={handleConfirmPasswordChange}
+                    placeholder="Confirm your password"
+                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="mb-2">
+                <label className="inline-flex items-center">
+                  <input
+                    type="checkbox"
+                    checked={rememberMe}
+                    onChange={handleRememberMeChange}
+                    className="rounded border-gray-300 text-blue-600 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 h-[17px] w-[17px] place-self-start"
+                  />
+                  <p className="ml-2 text-sm text-gray-700" style={{ fontSize: '12px' }}>
+                    By signing up, you agree to our
+                    <span className="text-[#007DFC]">Terms & Conditions</span> and
+                    <span className="text-[#007DFC]"> Privacy Policy</span>
+                  </p>
+                </label>
+              </div>
+
+              <button
+                type="submit"
+                className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 uppercase"
+              >
+                Submit
+              </button>
+            </form>
+
+            <div className="text-center mt-2">
+              <p className="text-sm text-gray-600">
+                Already have an account?
+                <Link href="/login" className="ml-1 text-blue-600 hover:underline">
+                  Login
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </AuthPageLayout>
+  );
+};
+
+export default Register;
